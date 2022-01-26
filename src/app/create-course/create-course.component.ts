87944cd3e4fe0a9781cad4cc357b9468a63c02ba
Uspehgp <uspehgp@gmail.com>
2022-01-26 21:30:52 +0300

    Create new Course Form for adding to firestore

--- src/app/create-course/create-course.component.ts
+++ src/app/create-course/create-course.component.ts
@@ -10,18 +10,58 @@ import firebase from 'firebase/app';
 import Timestamp = firebase.firestore.Timestamp;
 
 @Component({
-  selector: 'create-course',
-  templateUrl: 'create-course.component.html',
-  styleUrls: ['create-course.component.css']
+    selector: 'create-course',
+    templateUrl: 'create-course.component.html',
+    styleUrls: ['create-course.component.css']
 })
 export class CreateCourseComponent implements OnInit {
 
-  constructor() {
+    form = this.fb.group({
+        description: ['', Validators.required],
+        category: ['BEGINNER', Validators.required],
+        url: [''],
+        longDescription: ['', Validators.required],
+        promo: [false],
+        promoStartAt: [null]
+    });
 
-  }
+    constructor(private fb: FormBuilder) {
 
-  ngOnInit() {
+    }
 
-  }
+    ngOnInit() {
 
+    }
+
+    onCreateCourse() {
+
+        // const val = this.form.value;
+        //
+        // const newCourse: Partial<Course> = {
+        //     description: val.description,
+        //     url: val.url,
+        //     longDescription: val.longDescription,
+        //     promo: val.promo,
+        //     categories: [val.category]
+        // };
+
+        const newCourse = {...this.form.value} as Course;
+        newCourse.promoStartAt = Timestamp.fromDate(this.form.value.promoStartAt);
+        console.log('Created new course: ', newCourse);
+        // this.coursesService.createCourse(newCourse, this.courseId)
+        //     .pipe(
+        //         tap(course => {
+        //           console.log("Created new course: ", course);
+        //           this.router.navigateByUrl("/courses");
+        //         }),
+        //         catchError(err => {
+        //           console.log(err);
+        //           alert("Could not create the course.");
+        //           return throwError(err);
+        //         })
+        //     )
+        //     .subscribe();
+
+
+    }
 }
