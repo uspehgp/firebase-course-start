e61bfe1bc88ba506cc32bf37b1f79d7cc772d248
Uspehgp <uspehgp@gmail.com>
2022-01-28 19:45:47 +0300

    Building the Edit Course Dialog without saving

--- src/app/courses-card-list/courses-card-list.component.ts
+++ src/app/courses-card-list/courses-card-list.component.ts
@@ -1,9 +1,7 @@
-import {Component, EventEmitter, Input, OnInit, Output, ViewEncapsulation} from '@angular/core';
-import {Course} from "../model/course";
-import { MatDialog, MatDialogConfig } from "@angular/material/dialog";
-import {EditCourseDialogComponent} from "../edit-course-dialog/edit-course-dialog.component";
-import {catchError, tap} from 'rxjs/operators';
-import {throwError} from 'rxjs';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Course} from '../model/course';
+import {MatDialog, MatDialogConfig} from '@angular/material/dialog';
+import {EditCourseDialogComponent} from '../edit-course-dialog/edit-course-dialog.component';
 import {Router} from '@angular/router';
 
 @Component({
@@ -23,21 +21,21 @@ export class CoursesCardListComponent implements OnInit {
     courseDeleted = new EventEmitter<Course>();
 
     constructor(
-      private dialog: MatDialog,
-      private router: Router) {
+        private dialog: MatDialog,
+        private router: Router) {
     }
 
     ngOnInit() {
 
     }
 
-    editCourse(course:Course) {
+    editCourse(course: Course) {
 
         const dialogConfig = new MatDialogConfig();
 
         dialogConfig.disableClose = true;
         dialogConfig.autoFocus = true;
-        dialogConfig.minWidth = "400px";
+        dialogConfig.minWidth = '400px';
 
         dialogConfig.data = course;
 
--- src/app/edit-course-dialog/edit-course-dialog.component.ts
+++ src/app/edit-course-dialog/edit-course-dialog.component.ts
@@ -1,9 +1,7 @@
-import {Component, Inject, OnInit} from '@angular/core';
-import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
-import {Course} from "../model/course";
-import {FormBuilder, Validators, FormGroup} from "@angular/forms";
-import {AngularFireStorage} from '@angular/fire/storage';
-import {Observable} from 'rxjs';
+import {Component, Inject} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
+import {Course} from '../model/course';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 
 @Component({
@@ -13,7 +11,29 @@ import {Observable} from 'rxjs';
 })
 export class EditCourseDialogComponent {
 
-    constructor() {
+    form: FormGroup;
+    course: Course;
+
+    constructor(
+        private dialogRef: MatDialogRef<EditCourseDialogComponent>,
+        private fb: FormBuilder,
+        @Inject(MAT_DIALOG_DATA) course: Course
+    ) {
+        this.course = course;
+
+        this.form = this.fb.group({
+            description: [course.description, Validators.required],
+            longDescription: [course.longDescription, Validators.required],
+            promo: [course.promo]
+        });
+
+    }
+
+    close() {
+        this.dialogRef.close();
+    }
+
+    save() {
 
     }
 }
