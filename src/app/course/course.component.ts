a1601a0bd4482e0dee96e9f475fdb65fd79910c0
Uspehgp <uspehgp@gmail.com>
2022-01-30 17:48:14 +0300

    Implementing the Conclusion of Paginated Query

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -1,6 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {Course} from '../model/course';
+import {CoursesService} from '../services/courses.service';
+import {Lesson} from '../model/lesson';
+import {finalize} from 'rxjs/operators';
 
 
 @Component({
@@ -12,17 +15,38 @@ export class CourseComponent implements OnInit {
 
     course: Course;
 
+    lessons: Lesson[];
+
     loading = false;
+
+    lastPageLoaded = 0;
 
     displayedColumns = ['seqNo', 'description', 'duration'];
 
-    constructor(private route: ActivatedRoute) {
+    constructor(private route: ActivatedRoute,
+                private coursesService: CoursesService) {
 
     }
 
     ngOnInit() {
-
+        this.loading = true;
         this.course = this.route.snapshot.data['course'];
+        this.coursesService.findLessons(this.course.id)
+            .pipe(finalize(() => (this.loading = false)))
+            .subscribe(lessons => this.lessons = lessons);
     }
 
+    loadMore() {
+
+        this.lastPageLoaded++;
+
+        this.loading = true;
+
+        this.coursesService.findLessons(this.course.id, 'asc',
+            this.lastPageLoaded)
+            .pipe(
+                finalize(() => this.loading = false)
+            )
+            .subscribe(lessons => this.lessons = this.lessons.concat(lessons));
+    }
 }
